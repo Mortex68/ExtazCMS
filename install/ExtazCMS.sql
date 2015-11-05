d2d85884ed05674ca384231f9c5d7037a1c7bda8
TristanCode <trihonor@gmail.com>
2015-11-05 12:07:44 -0500

    Updated SQL file (ExtazCMS.sql)

--- install/ExtazCMS.sql
+++ install/ExtazCMS.sql
@@ -27,6 +27,27 @@ CREATE TABLE IF NOT EXISTS `extaz_votes` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;
 -- --------------------------------------------------------
 
+CREATE TABLE IF NOT EXISTS `extaz_permissions` (
+  `id` int(11) NOT NULL,
+  `uid` int(10) unsigned NOT NULL,
+  `admin` int(11) NOT NULL DEFAULT '0',
+  `add_user` int(11) NOT NULL DEFAULT '0',
+  `edit_user` int(11) NOT NULL DEFAULT '0',
+  `del_user` int(11) NOT NULL DEFAULT '0',
+  `add_code` int(11) NOT NULL DEFAULT '0',
+  `del_code` int(11) NOT NULL DEFAULT '0',
+  `add_page` int(11) NOT NULL DEFAULT '0',
+  `edit_page` int(11) NOT NULL DEFAULT '0',
+  `del_page` int(11) NOT NULL DEFAULT '0',
+  `add_news` int(11) NOT NULL DEFAULT '0',
+  `edit_news` int(11) NOT NULL DEFAULT '0',
+  `del_news` int(11) NOT NULL DEFAULT '0',
+  `add_faq` int(11) NOT NULL DEFAULT '0',
+  `del_faq` int(11) NOT NULL DEFAULT '0',
+  `edit_comment` int(11) NOT NULL DEFAULT '0',
+  `del_comment` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
 --
 -- Table structure for table `extaz_buttons`
 --
