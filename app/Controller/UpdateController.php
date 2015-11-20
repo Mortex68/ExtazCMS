804d67b56d8b662d9f625e5980c8e33a2917a37a
TristanCode <trihonor@gmail.com>
2015-11-20 09:36:38 -0500

    Updated UpdateController.php

--- app/Controller/UpdateController.php
+++ app/Controller/UpdateController.php
@@ -8,52 +8,37 @@
 class UpdateController extends AppController {
     public function admin_index() {
         if ($this->Auth->user('role') > 1) {
-            $cms_version = Configure::read('cms.version');
-            $cms_current_version = Configure::read('cms.current_version');
 
-                if ($cms_version != $cms_current_version) {
-                    //Format : @_path_filename.type.txt
-                    $server = file_get_contents("http://extaz-cms.fr/updates/index.php");
-                    $files = json_decode($server);
+                if ($this->version != $this->last_version) {
+                    $new_file = 'ExtazCMS_tmp.zip';
+                    $file = "http://extaz-cms.fr/cms/updates/files/ExtazCMS%20$this->version-$this->last_version.zip";
+                   // $sql  = file_get_contents("http://extaz-cms.fr/cms/updates/sql/ExtazCMS%20$this->version-$this->last_version.sql");
 
-                    foreach ($files as $file) {
-                        if (preg_match("/(@\w+)/u", $file)) {
-                            $file_contents = file_get_contents("http://extaz-cms.fr/updates/$file");
-                            $file = preg_replace("/@/", "", $file);
+                   // $db = ConnectionManager::getDataSource('default');
+                   // $db->rawQuery($sql);
 
-                            $file_name = preg_replace("/(_(.*)_)/u", "", $file);
-                            $file_name = preg_replace("/.txt/u", "", $file_name);
+                    if (!copy($file, $new_file)) {
+                        $this->Session->setFlash('Un problème est survenu !', 'error');
+                        //return $this->redirect(['controller' => 'update', 'action' => 'index', 'admin' => true]);
+                    } else {
+                        $zip = new ZipArchive;
+                        $res = $zip->open($new_file);
 
-                            $file_type = preg_replace("/(\w+\.)/u", "", $file_name);
-                            $file_type = preg_replace("/.txt/u", "", $file_type);
+                        if ($res === TRUE) {
+                            $zip->extractTo('../');
+                            $zip->close();
+                            unlink($new_file);
 
-                            $file_directory = preg_split("/_/u", $file);
-                            $file_directory = $file_directory[1];
-                            $file_directory = preg_replace("/-/", "/", $file_directory);
-                            $file_directory = ROOT . $file_directory;
-
-                            if ($file_type != "sql") {
-                                if (file_exists($file_directory)) {
-                                    $make_file = fopen($file_directory . $file_name, "w");
-                                    fwrite($make_file, $file_contents);
-                                    fclose($make_file);
-                                } else {
-                                    mkdir($file_directory);
-                                    $make_file = fopen($file_directory . $file_name, "w");
-                                    fwrite($make_file, $file_contents);
-                                    fclose($make_file);
-                                }
-                            } else {
-                                $db = ConnectionManager::getDataSource('default');
-                                $db->rawQuery($file_contents);
-                            }
+                            $this->Session->setFlash('Mise à jour effectué avec succès !', 'success');
+                            //return $this->redirect(['controller' => 'update', 'action' => 'index', 'admin' => true]);
+                        } else {
+                            $this->Session->setFlash('Un problème est survenu !', 'error');
+                            //return $this->redirect(['controller' => 'update', 'action' => 'index', 'admin' => true]);
                         }
                     }
-                    $this->Session->setFlash('Le cms a été mis à jour!', 'toastr_success');
-                    return $this->redirect(['controller' => 'update', 'action' => 'index', 'admin' => true]);
                 }
             } else {
-            throw new NotFoundException();
+                throw new NotFoundException();
         }
     }
 }
